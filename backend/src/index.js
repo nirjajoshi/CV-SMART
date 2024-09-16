db2711dc0517e4dce4c4d5ef8766bcc4e033311b
nirjajoshi <nirjajoshi01@gmail.com>
2024-09-16 15:41:20 +0530

    first setup

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({path:'./env'})   use this or below method or dotenv 
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+// Use this code or just go to db folder index.js use that .This gives a systematic look to main index.js becuase very few code is written here 
+
+// import express from "express"
+// import { error } from "console"
+// const app = express
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror",(error)=>{
+//             console.log("ERRROR",error);
+//             throw error 
+//         })
+//         app.listen(process.env.PORT,() =>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+        
+//     } catch (error) {
+//         console.error("ERROR",error)
+//         throw err 
+        
+//     }
+// })()
+
