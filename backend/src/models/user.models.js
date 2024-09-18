30a6908a21d980920a9d1a3e9c0acd26033a68af
nirjajoshi <nirjajoshi01@gmail.com>
2024-09-18 18:49:32 +0530

    first setup

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -50,6 +50,29 @@ userSchema.pre("save", async function(next) {
 // Password validation method
 userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
-};
+}
+userScehma.methods.generateAcessToken = function(){
+    return jwt.sign({
+        _id : this._id ,
+        email:this.email ,
+        fullname:this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userScehma.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+
+}
 
 export const User =mongoose.model("User",userSchema)
